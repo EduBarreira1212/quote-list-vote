9ccf5b009b002c3c3caf847d016b5277ebfc021e
Eduardo Barreira <edubarreira1212@gmail.com>
2024-03-14 11:22:17 -0300

    feat: removing unnecessary states and funtions

--- src/organisms/QuoteInput.jsx
+++ src/organisms/QuoteInput.jsx
@@ -1,19 +1,15 @@
 import { useEffect, useState } from "react";
 import Button from "../atoms/Button";
-import Input from "../atoms/Input";
 import Quote from "../atoms/Quote";
 import VoteButtons from "../atoms/VoteButtons";
 import axios from 'axios';
 
 
 const QuoteInput = () => {
-    const [author, setAuthor] = useState();
-    const [text, setText] = useState();
     const [note, setNote] = useState();
-    const [quote, setQuote] = useState({author: "", text: "", note: 0});
     const [quoteList, setQuoteList] = useState([]);
 
-    
+
     useEffect(() => {
         const fetchData = async () => {
             try {
@@ -27,28 +23,18 @@ const QuoteInput = () => {
     }, []);
 
 
-    const handleAuthorChange = (event) => {
-        setAuthor(event.target.value);
-    }
-
-    const handleTextChange = (event) => {
-        setText(event.target.value);
-    }
-
     const handleVoteClick = (event) => {
         setNote(event.target.value);
     }
 
     const handleClick = () => {
-        setQuote({author: author, text: text, note: note});
-        setQuoteList((prev) => [...prev, quote]);
+        //setQuoteList((prev) => [...prev, quote]);
     }
 
     return (
         <>
             <section className="quote-input-section hidden">
-                <Input className="input-author" onChange={handleAuthorChange} placeholder="Author"/>
-                <Input className="input-text" onChange={handleTextChange} placeholder="Text"/>
+                
                 <VoteButtons onClick={handleVoteClick}/>
                 <Button classname="btn" onClick={handleClick}>Send</Button>
                 {quoteList.sort((a, b) => b.note - a.note).map((item, index) => (
