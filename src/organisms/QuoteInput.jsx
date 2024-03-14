92908b3b2587a227458ed8e3e3ea85ad47f07581
Eduardo Barreira <edubarreira1212@gmail.com>
2024-03-14 12:29:09 -0300

    feat: state to manage the index and the setState created

--- src/organisms/QuoteInput.jsx
+++ src/organisms/QuoteInput.jsx
@@ -8,13 +8,15 @@ import axios from 'axios';
 const QuoteInput = () => {
     const [note, setNote] = useState();
     const [quoteList, setQuoteList] = useState([]);
-
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const response = await axios.get("https://type.fit/api/quotes");
                 setQuoteList(response.data);
+                setIsLoading(false)
             } catch (error) {
                 console.log("Error fetching data", error);
             }
@@ -22,6 +24,13 @@ const QuoteInput = () => {
         fetchData();
     }, []);
 
+    if (isLoading) {
+        return <div>Loading ...</div>
+    }
+
+    const nextItem = () => {
+        setCurrentIndex((prevIndex) => (prevIndex === quoteList.length - 1 ? 0 : prevIndex + 1));
+      };
 
     const handleVoteClick = (event) => {
         setNote(event.target.value);
@@ -29,12 +38,14 @@ const QuoteInput = () => {
 
     const handleClick = () => {
         //setQuoteList((prev) => [...prev, quote]);
+        console.log(quoteList[currentIndex]);
+        nextItem();
     }
 
     return (
         <>
             <section className="quote-input-section hidden">
-                
+                <Quote>{quoteList[currentIndex]}</Quote>
                 <VoteButtons onClick={handleVoteClick}/>
                 <Button classname="btn" onClick={handleClick}>Send</Button>
                 {quoteList.sort((a, b) => b.note - a.note).map((item, index) => (
