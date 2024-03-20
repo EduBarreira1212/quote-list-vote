a41de680d2a40bac05f69ee0b3d8e0460560dbea
Eduardo Barreira <edubarreira1212@gmail.com>
2024-03-19 21:29:18 -0300

    feat: obsolet file deleted

--- src/atoms/Input.jsx
+++
@@ -1,8 +0,0 @@
-
-
-const Input = ({onChange, className, placeholder}) => {
-
-    return <input type="text" onChange={onChange} className={className} placeholder={placeholder}/>
-}
-
-export default Input;
