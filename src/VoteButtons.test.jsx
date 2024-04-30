12726937862b45a926839422ac14e5500b5ada4f
Eduardo Barreira <edubarreira1212@gmail.com>
2024-04-30 09:23:15 -0300

    test: new test added

---
+++ src/VoteButtons.test.jsx
@@ -0,0 +1,12 @@
+import '@testing-library/jest-dom';
+import { fireEvent, render, screen } from '@testing-library/react'
+import VoteButtons from './atoms/VoteButtons';
+
+
+test("click on button", () => {
+    const mockFunction = jest.fn()
+    render(<VoteButtons onClick={mockFunction}/>)
+    const btn = screen.getByText("2");
+    fireEvent.click(btn);
+    expect(mockFunction).toHaveBeenCalledTimes(1);
+})
