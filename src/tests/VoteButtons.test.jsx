cee7c91f58ca954cf8a11a956c53df6545141510
Eduardo Barreira <edubarreira1212@gmail.com>
2024-05-01 18:30:46 -0300

    test: new test structure

--- src/tests/VoteButtons.test.jsx
+++ src/tests/VoteButtons.test.jsx
@@ -2,11 +2,17 @@ import '@testing-library/jest-dom';
 import { fireEvent, render, screen } from '@testing-library/react'
 import VoteButtons from '../atoms/VoteButtons';
 
-
-test("click on button", () => {
-    const mockFunction = jest.fn()
-    render(<VoteButtons onClick={mockFunction}/>)
-    const btn = screen.getByText("2");
-    fireEvent.click(btn);
-    expect(mockFunction).toHaveBeenCalledTimes(1);
+describe("Vote buttons component", () => {
+    test("render a button", () => {
+        render(<VoteButtons/>)
+        const btn = screen.getByText("10")
+        expect(btn).toBeInTheDocument()
+    })
+    test("click on button", () => {
+        const mockFunction = jest.fn()
+        render(<VoteButtons onClick={mockFunction}/>)
+        const btn = screen.getByText("2");
+        fireEvent.click(btn);
+        expect(mockFunction).toHaveBeenCalledTimes(1);
+    })
 })
