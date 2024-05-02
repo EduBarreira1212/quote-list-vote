b8ca5e839b6eb33217757ac98c2a03d9b1fbec41
Eduardo Barreira <edubarreira1212@gmail.com>
2024-05-02 11:33:40 -0300

    test: adding test to render loading message

--- src/tests/QuoteInput.test.jsx
+++ src/tests/QuoteInput.test.jsx
@@ -3,6 +3,11 @@ import { fireEvent, render, screen } from '@testing-library/react'
 import QuoteInput from "../organisms/QuoteInput"
 
 describe("QuoteInput component", () => {
+    test("render loading message", () => {
+        render(<QuoteInput/>)
+        const loading = screen.getByText("Loading ...")
+        expect(loading).toBeInTheDocument();
+    })
     test("render component", async () => {
         render(<QuoteInput/>);
         const button = await screen.findByText("Send")
